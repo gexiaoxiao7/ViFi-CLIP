63fcec2088df90897a02e42c55626456fd580067
gexiaoxiao7 <1004083066@qq.com>
2024-03-27 22:01:27 +0800

    update

--- docs/INSTALL.md
+++ docs/INSTALL.md
@@ -7,9 +7,10 @@ This codebase is tested on Ubuntu 20.04.2 LTS with python 3.8. Follow the below 
 # Create a conda environment
 conda create -y -n vclip python=3.7
 # Activate the environment
+source activate
 conda activate vclip
 # Install requirements
-pip install -r requirements.txt
+pip install requirements.txt
 ```
 
 * Install Apex for enabling mixed-precision training.
@@ -18,5 +19,6 @@ NOTE: Make sure to have system CUDA of same version as of PyTorch CUDA version t
 ```
 git clone https://github.com/NVIDIA/apex
 cd apex
+pip install -v --no-cache-dir --no-build-isolation --global-option="--cpp_ext" --global-option="--cuda_ext" --global-option="--deprecated_fused_adam" --global-option="--xentropy" --global-option="--fast_multihead_attn" ./
 pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 ```
--- main.py
+++ main.py
@@ -248,19 +248,7 @@ if __name__ == '__main__':
     # prepare config
     args, config = parse_option()
 
-    # init_distributed
-    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
-        rank = int(os.environ["RANK"])
-        world_size = int(os.environ['WORLD_SIZE'])
-        print(f"RANK and WORLD_SIZE in environ: {rank}/{world_size}")
-    else:
-        rank = -1
-        world_size = -1
-    torch.cuda.set_device(args.local_rank)
-    torch.distributed.init_process_group(backend='nccl', init_method='env://', world_size=world_size, rank=rank)
-    torch.distributed.barrier(device_ids=[args.local_rank])
-
-    seed = config.SEED + dist.get_rank()
+    seed = config.SEED
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
@@ -270,12 +258,11 @@ if __name__ == '__main__':
     Path(config.OUTPUT).mkdir(parents=True, exist_ok=True)
 
     # logger
-    logger = create_logger(output_dir=config.OUTPUT, dist_rank=dist.get_rank(), name=f"{config.MODEL.ARCH}")
+    logger = create_logger(output_dir=config.OUTPUT, name=f"{config.MODEL.ARCH}")
     logger.info(f"working dir: {config.OUTPUT}")
 
-    # save config 
-    if dist.get_rank() == 0:
-        logger.info(config)
-        shutil.copy(args.config, config.OUTPUT)
+    # save config
+    logger.info(config)
+    shutil.copy(args.config, config.OUTPUT)
 
     main(config)
